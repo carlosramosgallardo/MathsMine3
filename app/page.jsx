332201d71af68ef2cf9ed8c1391b4b1519034e56
crg <botsandpods@gmail.com>
2025-04-20 11:09:01 +0200

    seo

--- app/page.jsx
+++ app/page.jsx
@@ -80,8 +80,7 @@ export default function Page() {
           <section className="mb-12 text-center">
             <h1 className="text-xl font-semibold mt-8 mb-2">Master Math and Shape the Future with MathsMine3</h1>
             <p className="text-base text-gray-500 text-center mb-2">
-              MathsMine3 is a unique, open-source Web3 experiment where your brainpower mines MM3 tokens.
-              Solve math puzzles and help shape the value of the MM3 token through Proof of Vote (PoV) and Proof of Ask (PoA).
+            MathsMine3 is a free-to-play, open-source, and unique Web3 experiment where you solve math puzzles and earn MM3 — a fake token with no real-world value, used exclusively within MathsMine3 to participate in Proof of Ask (PoA) and Proof of Vote (PoV).
             </p>
           </section>
 
